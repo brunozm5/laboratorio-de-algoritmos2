85fc1f3b2a64c594fbca0b64635dc4f74a541796
Bruno Zanchi Machry <bruno.z.machry@gmail.com>
2024-08-17 12:05:57 -0300

    Arquivos

--- matriz/exemplo1.py
+++ matriz/exemplo1.py
@@ -6,15 +6,16 @@ def multiplicar_matriz(matriz_informada, numero_escalar):
     return matriz_informada
 
 matriz = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25],
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
 ]
 
-for linha in range(len(matriz)):
-    for coluna in range(len(matriz[linha])):
-        print(matriz[linha][coluna], end="\t")
-    print("")
-        
+resultado = multiplicar_matriz(matriz, 2)
+
+for linha in range(len(resultado)):
+  for coluna in range(len(resultado[linha])):
+    print(resultado[linha][coluna], end="\t")
+  print("")
